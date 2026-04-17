2c2e95af6cd5c68b440655aa12142c070f3a8f10
Petya Markova <petya.n.markova@gmail.com>
2026-04-17 16:56:07 +0300

    Revert "fix(uxc-integration): Enabled .d.ts generation for seamless wrappers"

--- webapp/Component.ts
+++ webapp/Component.ts
@@ -2,6 +2,7 @@ import UIComponent from "sap/ui/core/UIComponent";
 import models from "uxc/integration/model/models";
 import Device from "sap/ui/Device";
 
+import "@ui5/webcomponents/dist/Assets";
 import "@ui5/webcomponents-fiori/dist/Assets";
 
 /**
